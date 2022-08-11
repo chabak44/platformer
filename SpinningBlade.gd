e6b3b17b842974e0cabe6af2e4aba947eeff539c
chabak44 <108499939+chabak44@users.noreply.github.com>
2022-08-11 14:56:34 +1200

    sd

--- SpinningBlade.gd
+++ SpinningBlade.gd
@@ -5,3 +5,11 @@ func _ready():
 
 func _process(delta):
 	$Path2D/PathFollow2D.offset += follow_speed * delta
+
+
+func _on_Area2D_body_entered(body):
+	print(body)
+	if body.is_in_group("player"):
+		print("Player dead")
+		if GameStats.check_reset() == false:
+			body.global_position =GameStats.get_spawn().global_position
